780da8917454ad012301e0b178b66409ef42d8f0
Rger <mckeenweb@gmail.com>
2023-03-21 21:32:24 +0800

    clone repo

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use(express.json());
 
 // routes
 
-app.use('/api/v1/tasks', tasks);
+app.use('/api/v1/tasks', tasks)
 
 app.use(notFound);
-app.use(errorHandlerMiddleware);
+app.use(errorHandlerMiddleware)
 const port = process.env.PORT || 5000;
 
 const start = async () => {
