2513d1d4dd9f68a7c8dc2443619af855cad00767
KeenGWatanabe <mckeenweb@gmail.com>
2025-07-04 21:25:58 +0800

    revert

--- db/connect.js
+++ db/connect.js
@@ -8,10 +8,5 @@ return mongoose.connect(url, {
     useUnifiedTopology: true,
 })
 }
-// Example Express.js endpoint
-app.get('/healthz', (req, res) => {
-  // Add database connection check if needed
-  res.status(200).send('OK');
-});
 
 module.exports = connectDB
